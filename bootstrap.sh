8eff12e9903166a3df5eac8ee2fa062f234c6059
Maykel Santos Braz <maykelsb@yahoo.com.br>
2016-03-30 14:06:00 -0300

    Update bootstrap.sh
    instalação do composer

--- bootstrap.sh
+++ bootstrap.sh
@@ -21,3 +21,8 @@ sudo systemctl start httpd
 echo ">>> Instalando o PHP5.6"
 sudo yum -y install php56w php56w-opcache php56w-intl php56w-pdo php56w-pecl-xdebug php56w-xml php56w-mbstring php56w-process
 
+echo ">>> Instalando o composer"
+sudo php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+sudo php -r "if (hash('SHA384', file_get_contents('composer-setup.php')) === '41e71d86b40f28e771d4bb662b997f79625196afcca95a5abf44391188c695c6c1456e16154c75a211d238cc3bc5cb47') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+sudo php composer-setup.php --install-dir=/usr/bin
+sudo php -r "unlink('composer-setup.php');"
