5fbb8f738f9a1fb9ffdeb0b44aa583ba5f099c15
Maykel Santos Braz <maykelsb@yahoo.com.br>
2016-04-06 17:54:49 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -47,3 +47,5 @@ echo ">>> Instalando o instalador do Symfony"
 sudo curl -LsS https://symfony.com/installer -o /usr/local/bin/symfony
 sudo chmod a+x /usr/local/bin/symfony
 
+# Definir o timezone do php
+# Criar vhosts (??)
