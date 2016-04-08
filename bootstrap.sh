034458baedefbfa39c26ade3de7fedeb6429cb31
Maykel Santos Braz <maykelsb@yahoo.com.br>
2016-04-08 14:37:19 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,6 +10,9 @@ echo ">>> Adicionando novos repositorios ao yum - php56w"
 sudo rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 sudo rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 
+# yum makecache fast
+
+
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
 echo ">>> Atualizando os pacotes atualmente instalados"
 sudo yum update -y
