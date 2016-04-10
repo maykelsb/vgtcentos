7739be6c2d16846c9a018028582672b2d52d68ae
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-04-10 17:43:56 -0300

    Criando o projeto sem sudo; ajustando permissões dos diretórios; removendo bloqueio de acesso app_dev e config; rodando os requerimentos do projeto

--- scripts/symfony/start.sh
+++ scripts/symfony/start.sh
@@ -8,11 +8,22 @@ if [ -d /var/www/$1 ]; then
   sudo rm -rf /var/www/$1
 fi
 echo ">>> Criando o projeto em /var/www/${1}..."
-sudo symfony new /var/www/$1
+symfony new /var/www/$1
+
 echo ">>> Ajustando permissões do diretório..."
-sudo chown -R vagrant:apache /var/www/$1
+sudo chown -R vagrant:vagrant /var/www/$1
 sudo chmod -R 775 /var/www/$1
 
+# https://www.centos.org/forums/viewtopic.php?t=6834
+echo ">>> Corrigindo o contexto selinux"
+sudo restorecon -r /var/www/$1
+
 echo ">>> Executando php /var/www/${1}bin/symfony_requirements"
-sudo php /var/www/${1}/bin/symfony_requirements
+sudo php /var/www/$1/bin/symfony_requirements
+
+echo ">>> Tirando restrições de acesso do app_dev.php"
+sed --in-place '10,19d' /var/www/$1/web/app_dev.php
+
+echo ">>> Tirando restrições de acesso do config.php"
+sed --in-place '13,23d' web/config.php
 echo "==============================================================================="
