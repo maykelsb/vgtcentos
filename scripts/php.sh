bbc826adf59cb02bd25937aeafb3691273bfdb07
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-04-10 17:42:12 -0300

    Ligando a exibição de erros; mudando a forma de reiniciar o apache

--- scripts/php.sh
+++ scripts/php.sh
@@ -4,17 +4,19 @@
 echo "==============================================================================="
 echo ">>> Instalando o PHP5.6"
 sudo yum -y install php56w php56w-opcache php56w-intl php56w-pdo php56w-pecl-xdebug php56w-xml php56w-mbstring php56w-process
-echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Configurando a Timezone do php para ${1}"
 sudo sed -i 's/;date.timezone =/date.timezone = America\/Sao_Paulo/g' /etc/php.ini
-echo "==============================================================================="
+
+echo ">>> Configurando a Timezone do php para ${1}"
+sudo sed -i 's/display_errors = Off/display_errors = On/g' /etc/php.ini
 
 # Se o apache estiver instalado, reinicia-o
 if type httpd > /dev/null; then
-  echo "==============================================================================="
   echo ">>> Reiniciando o apache"
-  sudo apachectl restart
-  echo "==============================================================================="
+  sudo httpd -k restart
 fi
+echo "==============================================================================="
+
+# @todo Setar as configurações do PHP para development
+
