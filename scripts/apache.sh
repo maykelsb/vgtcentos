fb2fa682ca02c3952c8b5a9e972f65f721ae00c8
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-04-10 17:39:07 -0300

    Removendo a modificação no grupo do vagrant/apache. Trocando o usuário do apache para vagrant/vagrant. Definindo servername do servidor

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -3,22 +3,23 @@
 echo "==============================================================================="
 echo ">>> Instalando apache"
 sudo yum -y install httpd httpd-devel
-echo "==============================================================================="
 
-echo "==============================================================================="
-echo ">>> Adicionando o usuário vagrant ao grupo do apache"
-sudo usermod -a -G apache vagrant
-echo "==============================================================================="
+#echo ">>> Adicionando o usuário vagrant ao grupo do apache"
+#sudo usermod -a -G apache vagrant
 
-echo "==============================================================================="
+echo "Trocando o usuário do apache para vagrant:vagrant"
+sudo sed -i 's/Group apache/Group vagrant/g' /etc/httpd/conf/httpd.conf
+sudo sed -i 's/User apache/User vagrant/g' /etc/httpd/conf/httpd.conf
+
+echo ">>> Configurando o ServerName para ${1}"
+sudo sed -i 's/#ServerName www.example.com/ServerName ${1}:80/g' /etc/httpd/conf/httpd.conf
+
 echo ">>> Ativando inicializacao automatica do apache"
 sudo systemctl enable httpd
 sudo systemctl start httpd
-echo "==============================================================================="
 
 # Configuração geral de vhosts
 # fonte: https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-centos-7
-echo "==============================================================================="
 echo ">>> Configurando vhost"
 sudo mkdir /etc/httpd/sites-available
 sudo mkdir /etc/httpd/sites-enabled
