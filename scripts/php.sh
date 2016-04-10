265d6d3e4730f9ca8d9d04d9ae1f4513cd3ba813
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-04-10 05:35:56 -0300

    Correção da chamada de sed. Reinicialização do apache, se estiver instalado

--- scripts/php.sh
+++ scripts/php.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# @todo Verificar a necessidade de alterar o DirectoryIndex index.html
 
 echo "==============================================================================="
 echo ">>> Instalando o PHP5.6"
@@ -6,6 +7,14 @@ sudo yum -y install php56w php56w-opcache php56w-intl php56w-pdo php56w-pecl-xde
 echo "==============================================================================="
 
 echo "==============================================================================="
-echo ">>> Configurando a Timezone do apache para ${1}"
-sudo sed -i -- 's/date.timezone = /date.timezone = America/Sao_Paulo/g' /etc/php.ini    ## files ending in .baz
+echo ">>> Configurando a Timezone do php para ${1}"
+sudo sed -i 's/;date.timezone =/date.timezone = America\/Sao_Paulo/g' /etc/php.ini
 echo "==============================================================================="
+
+# Se o apache estiver instalado, reinicia-o
+if type httpd > /dev/null; then
+  echo "==============================================================================="
+  echo ">>> Reiniciando o apache"
+  sudo apachectl restart
+  echo "==============================================================================="
+fi
