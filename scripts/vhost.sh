25620e1d980f9dc2b7d8fbe60d038d6f02aea831
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-04-10 17:41:04 -0300

    Trocando o caminho dos logs; removendo alteração de usuário

--- scripts/vhost.sh
+++ scripts/vhost.sh
@@ -5,35 +5,21 @@
 echo "==============================================================================="
 echo ">>> Criando a pasta do projeto (/var/www/${1}) e alterando permissões"
 sudo mkdir -p /var/www/$1/web
-sudo chown -R vagrant:apache /var/www/$1
+sudo chown -R vagrant:vagrant /var/www/$1
 sudo chmod -R 775 /var/www
-echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Criando página demo"
 touch /var/www/$1/web/index.html
 echo "Bem vindo a ${1}" >> /var/www/$1/web/index.html
-echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Criando pasta de logs (/var/www/log/${1})"
-sudo mkdir -p /var/www/log/$1
-echo "==============================================================================="
+sudo mkdir -p /var/log/httpd/$1
 
-echo "==============================================================================="
 echo ">>> Criando arquivos de log: error.log e requests.log"
-touch /var/www/log/$1/error.log
-touch /var/www/log/$1/requests.log
-echo "==============================================================================="
-
-echo "==============================================================================="
-echo ">>> Alterando permissões da pasta de log"
-sudo chown -R vagrant:apache /var/www/log/$1
-sudo chmod -R 775 /var/www/log/$1
-echo "==============================================================================="
+touch /var/log/httpd/$1_error.log
+touch /var/log/httpd/$1_requests.log
 
 # @todo: Verificar a existência desses arquivos antes de criá-los
-echo "==============================================================================="
 echo ">>> Criando arquivo /etc/httpd/sites-available/${1}.conf"
 if [ -f /etc/httpd/sites-available/$1.conf ]; then
     echo "ERRO: O arquivo /etc/httpd/sites-available/${1}.conf já existe. Abortando o provisionamento do VHOST"
@@ -47,14 +33,10 @@ sudo echo "    DocumentRoot /var/www/${1}/web" >> /etc/httpd/sites-available/$1.
 sudo echo "    ErrorLog /var/www/log/${1}/error.log" >> /etc/httpd/sites-available/$1.conf
 sudo echo "    CustomLog /var/www/log/${1}/requests.log combined" >> /etc/httpd/sites-available/$1.conf
 sudo echo "</VirtualHost>" >> /etc/httpd/sites-available/$1.conf
-echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Habilitando vhost /etc/httpd/sites-enabled/${1}.conf"
 sudo ln -s /etc/httpd/sites-available/$1.conf /etc/httpd/sites-enabled/$1.conf
-echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Reiniciando o apache"
-sudo apachectl restart
+sudo httpd -k restart
 echo "==============================================================================="
