5b9d7b1bc656f5282b12ef2fa7e1615127225ba2
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-04-10 06:01:08 -0300

    Correção da mensagem de verificação do npm

--- scripts/bower.sh
+++ scripts/bower.sh
@@ -4,8 +4,7 @@ echo "==========================================================================
 echo ">>> Instalando o bower"
 echo ">>> Verificando se o NPM está instalado"
 if ! type npm > /dev/null; then
-    RED='\033[0;31m'
-    printf("${RED}NPM não está instalado. Abortando o provisionamento do bower")
+    echo "NPM não está instalado. Abortando o provisionamento do bower"
     exit 1
 fi
 echo ">>> Procedendo com a instalação do bower"
