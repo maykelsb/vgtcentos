aedf9716c005dd815ba9d557e622368af43f2a5c
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-04-10 03:11:13 -0300

    Formatação dos comandos

--- scripts/apache.sh
+++ scripts/apache.sh
@@ -2,15 +2,18 @@
 echo "==============================================================================="
 echo ">>> Instalando apache"
 sudo yum -y install httpd httpd-devel
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Ativando inicializacao automatica do apache"
 sudo systemctl enable httpd
 sudo systemctl start httpd
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Adicionando o usuário vagrant ao grupo do apache"
 sudo usermod -a -G apache vagrant
+echo "==============================================================================="
 
 # Configuração geral de vhosts
 # fonte: https://www.digitalocean.com/community/tutorials/how-to-set-up-apache-virtual-hosts-on-centos-7
@@ -19,3 +22,4 @@ echo ">>> Configurando vhost"
 sudo mkdir /etc/httpd/sites-available
 sudo mkdir /etc/httpd/sites-enabled
 sudo echo "IncludeOptional sites-enabled/*.conf" >> /etc/httpd/conf/httpd.conf
+echo "==============================================================================="
--- scripts/vhost.sh
+++ scripts/vhost.sh
@@ -8,25 +8,30 @@ echo ">>> Criando a pasta do projeto (/var/www/${1}) e alterando permissões"
 sudo mkdir -p /var/www/$1/web
 sudo chown -R vagrat:apache /var/www/$1
 sudo chmod -R 755 /var/www
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Criando página demo"
 touch /var/www/$1/web/index.php
 echo "Bem vindo a ${1}" >> /var/www/$1/web/index.php
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Criando pasta de logs (/var/www/log/${1})"
 sudo mkdir -p /var/www/log/$1
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Criando arquivos de log: error.log e requests.log"
-sudo touch /var/www/log/$1/error.log
-sudo touch /var/www/log/$1/requests.log
+touch /var/www/log/$1/error.log
+touch /var/www/log/$1/requests.log
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Alterando permissões da pasta de log"
 sudo chown -R vagrat:apache /var/www/log/$1
 sudo chmod -R 755 /var/www/log/$1
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Criando arquivo /etc/httpd/sites-available/${1}.conf"
@@ -38,11 +43,14 @@ sudo echo "    DocumentRoot /var/www/${1}/web" >> /etc/httpd/sites-available/$1.
 sudo echo "    ErrorLog /var/www/log/${1}/error.log" >> /etc/httpd/sites-available/$1.conf
 sudo echo "    CustomLog /var/www/log/${1}/requests.log combined" >> /etc/httpd/sites-available/$1.conf
 sudo echo "</VirtualHost>" >> /etc/httpd/sites-available/$1.conf
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Habilitando vhost /etc/httpd/sites-enabled/${1}.conf"
 sudo ln -s /etc/httpd/sites-available/$1.conf /etc/httpd/sites-enabled/$1.conf
+echo "==============================================================================="
 
 echo "==============================================================================="
 echo ">>> Reiniciando o apache"
 sudo apachectl restart
+echo "==============================================================================="
