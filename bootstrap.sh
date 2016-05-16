44da0f0e2607442f2866169d374a0baf804b2f9a
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-05-16 18:30:17 -0300

    Desligando o SELinux

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,17 +10,13 @@ echo "==========================================================================
 echo ">>> Desligando o firewall"
 sudo systemctl disable firewalld
 sudo systemctl stop firewalld
-echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Adicionando novos repositorios ao yum"
 sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm http://mirror.webtatic.com/yum/el7/webtatic-release.rpm
-echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Atualizando metadatas dos repositórios yum"
 sudo yum makecache fast
-echo "==============================================================================="
+
 
 # Está quebrando a montagem de /vagrant
 #echo "==============================================================================="
@@ -28,7 +24,9 @@ echo "==========================================================================
 #sudo yum update -y
 #echo "==============================================================================="
 
-echo "==============================================================================="
 echo ">>> Instalando wget e curl"
 sudo yum -y install wget curl
+
+echo ">>> Desabilitando o SELinux"
+sudo sed -i 's/SELINUX=permissive/SELINUX=disabled/g' /etc/sysconfig/selinux
 echo "==============================================================================="
