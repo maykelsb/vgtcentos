069fb3a9465647caccd7f88e70d36f0e8dc96e70
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-05-24 13:05:52 -0300

    Incluíndo mariadb

---
+++ scripts/mariadb.sh
@@ -0,0 +1,14 @@
+
+echo "==============================================================================="
+echo ">>> Instalando os pacotes mariadb e mariadb-server"
+sudo yum install -y mariadb-server mariadb
+
+echo ">>> Verificando se tem o PHP instalado para adicionar o drive de comunicação"
+if type php > /dev/null; then
+    echo ">>> PHP instalado. Instalando php56w-mysqlnd"
+    sudo yum install -y php56w-mysqlnd
+
+    echo ">>> Reiniciando o apache"
+    sudo httpd -k restart
+fi
+echo "==============================================================================="
