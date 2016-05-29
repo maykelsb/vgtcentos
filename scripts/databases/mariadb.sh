122dec1c66501dd47b6d1bd5836489a79120f515
Maykel S. Braz <maykelsb@yahoo.com.br>
2016-05-29 15:26:54 -0300

    Removendo duplicidade do maridb.sh

--- scripts/databases/mariadb.sh
+++ scripts/databases/mariadb.sh
@@ -7,6 +7,16 @@ echo ">>> Iniciando e instalando o serviço do mariadb"
 sudo systemctl start mariadb;
 sudo systemctl enable mariadb;
 
+echo ">>> Criando a base de dados ${1}"
+mysql -uroot -e "CREATE DATABASE ${1} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+
+echo ">>> Criando o usuario ${1} com senha '${1}'"
+mysql -uroot -e "CREATE USER ${1}@localhost IDENTIFIED BY '${1}';"
+
+echo ">>> Definindo permissões"
+mysql -uroot -e "GRANT ALL PRIVILEGES ON ${1}.* to '${1}'@'%';"
+mysql -uroot -e "FLUSH PRIVILEGES;"
+
 echo ">>> Verificando se tem o PHP instalado para adicionar o drive de comunicação"
 if type php > /dev/null; then
     echo ">>> PHP instalado. Instalando php56w-mysqlnd"
